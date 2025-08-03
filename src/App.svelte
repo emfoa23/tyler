7ca8e049b619dbe1f652eb4b53a373d086422970
brian-ahn <brian.ahn@navercorp.com>
2025-08-03 13:48:14 +0900

    스타일 수정

--- src/App.svelte
+++ src/App.svelte
@@ -6,13 +6,13 @@
   }
 
   const getUniqueRandomNumbers = () => {
-    const numbers = new Set();
+    const numbers = new Set()
 
     while (numbers.size < 6) {
-      numbers.add(Math.floor(Math.random() * 45) + 1);
+      numbers.add(Math.floor(Math.random() * 45) + 1)
     }
 
-    return [...numbers].sort((a, b) => a - b);
+    return [...numbers].sort((a, b) => a - b)
   }
 
   const getBackGroundColor = (number) => {
@@ -24,11 +24,11 @@
   }
 </script>
 
-<div class="container mx-auto h-[90vh] flex flex-col">
+<div class="flex h-[90vh] flex-col">
   <div class="flex-none p-4">
-    <h1 class="text-2xl font-bold mb-4">로또번호 생성기</h1>
+    <h1 class="mb-4">로또번호 생성기</h1>
     <div class="mb-4">
-      <button onclick={appendNumber} class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">로또번호 생성</button>
+      <button onclick={appendNumber}>로또번호 생성</button>
     </div>
   </div>
 
